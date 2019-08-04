2012a01a9680ea1b0311dc176dcb1e3757baca66
[your <leigh.tm@gmail.com>
2019-08-04 03:46:21 -0400

    fix cell phone view

--- js/about.js
+++ js/about.js
@@ -1,1 +1,10 @@
-!
+$('#toggle').click(function() {
+  $(this).toggleClass('active');
+  $('#overlay').toggleClass('open');
+ });
+
+ var touch = Modernizr.touch;
+$('.img-holder').imageScroll({
+  imageAttribute: (touch === true) ? 'image-mobile' : 'image',
+  touch: touch
+});
